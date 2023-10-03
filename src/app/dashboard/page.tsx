bac8aef48da730d7c95b2ff01eb06a06098b675d
stefan022 <stefan022github@gmail.com>
2023-10-03 12:23:43 +0200

    error handling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,26 @@
+'use client';
+
 import { FC } from 'react'
 
 import DashboardLayout from './layout'
 import Link from 'next/link'
+import Error from './error'
+import { ErrorBoundary } from '@/error/ErrorBoundary'
+import { resetFunc } from '@/helpers/resetFunc'
 
 const Dashboard: FC = (): JSX.Element => {
     return (
         <DashboardLayout>
-            <p>Welcome to the Dashboard</p>
-            <Link href={"/dashboard/products"}>Products</Link>
-            <Link href={"/dashboard/settings"}>Settings</Link>
+            <ErrorBoundary fallback={ 
+                <Error 
+                    error={"Something went wrong!"} 
+                    reset={resetFunc}
+                /> 
+            }>
+                <p>Welcome to the Dashboard</p>
+                <Link href={"/dashboard/products"}>Products</Link>
+                <Link href={"/dashboard/settings"}>Settings</Link>
+            </ErrorBoundary>
         </DashboardLayout>
     )
 }
