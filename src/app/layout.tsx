bc6c445245f012e573afb3da55d8663e80c6e02a
stefan022 <stefan022github@gmail.com>
2023-10-08 02:40:14 +0200

    scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { FC, ReactNode } from "react"
 import { exo_2 } from "@/constants/googleFont/exo_2"
+import Script from "next/script";
+import { onLoad } from "@/script/onLoad";
 
 import "../scss/main.scss"
 
@@ -11,6 +15,10 @@ const RootLayout: FC<IProps> = ({ children }): JSX.Element => {
     return (
         <html lang="en">
             <body className={`${exo_2.variable}`}>{children}</body>
+            <Script
+                src="https://code.jquery.com/jquery-3.6.0.min.js"
+                onLoad={onLoad}
+            />
         </html>
     ) 
 }
