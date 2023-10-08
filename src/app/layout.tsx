cce6531e197b9cfa5898a71dcacd8981df3fbcb3
stefan022 <stefan022github@gmail.com>
2023-10-08 02:41:24 +0200

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Script from "next/script";
 import { onLoad } from "@/script/onLoad";
 
 import "../scss/main.scss"
+import { metadata } from "@/constants/metadata";
 
 interface IProps {
     children: ReactNode;
@@ -14,6 +15,10 @@ interface IProps {
 const RootLayout: FC<IProps> = ({ children }): JSX.Element => {
     return (
         <html lang="en">
+             <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>{metadata.title as string}</title>
+            </head>
             <body className={`${exo_2.variable}`}>{children}</body>
             <Script
                 src="https://code.jquery.com/jquery-3.6.0.min.js"
