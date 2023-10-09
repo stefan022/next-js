ba3ee777859be19a8cea23d1ca778dc8a31782df
stefan022 <stefan022github@gmail.com>
2023-10-09 20:12:57 +0200

    compress

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withMDX = require('@next/mdx')()
 const nextConfig = {
     skipTrailingSlashRedirect: true,
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    compress: false,
 }
 
 module.exports = withMDX(nextConfig);
